6591ab1bb6e28404ad2fa1b528474fc80e976677
Rômulo Cadó Dorneles <79316323+RomuloCado@users.noreply.github.com>
2021-04-19 11:21:55 -0300

    Create exercicio03_pilha.py

---
+++ exercicio03_pilha.py
@@ -0,0 +1,5 @@
+'''
+Escreva um algoritmo, usando uma Pilha, que inverte as letras de cada palavra de um texto terminado por ponto (.) preservando a ordem das palavras. Por exemplo, dado o texto: ESTE EXERCÍCIO É MUITO FÁCIL. 
+a.	A saída deve ser: ETSE OICÍCREXE É OTIUM LICÁF
+
+'''
