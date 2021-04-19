62084047175cead0f767e13c42be25a7fa0dd335
Rômulo Cadó Dorneles <79316323+RomuloCado@users.noreply.github.com>
2021-04-19 11:10:52 -0300

    Create exercicio01_lista_nao_ordenada.py

---
+++ exercicio01_lista_nao_ordenada.py
@@ -0,0 +1,8 @@
+'''
+1.	Crie um novo método INSERE_UNICO, nesse método não será permitido números repetidos
+2.	Crie o método DELETADOR_FULMINANTE, esse método elimina todas as ocorrências de um número dado       
+3.	Crie o método DELETADOR_CALMINHO, esse método elimina  as ocorrências de um número dado (até aqui é igual ao método EXCLUIR), 
+    mas se existirem mais ocorrências ele deixa apenas a primeira ocorrência 
+    2,3,56,4,9,3,45 --> 2,3,56,4,9,45 
+    2,3,56,4,9,45 --> 2,56,4,9,45
+'''
