46797b2f378660d845c0022d1192246b7c1c2bfd
Rômulo Cadó Dorneles <79316323+RomuloCado@users.noreply.github.com>
2021-04-19 11:14:27 -0300

    Create exercicio02_lista_ordenada.py

---
+++ exercicio02_lista_ordenada.py
@@ -0,0 +1,5 @@
+'''
+	Crie um novo método INSERE_UNICO para a lista ordenada, nesse método não será permitido números repetidos
+	Crie um método IMPRIME_INVERSO, ou seja: do maior valor para o menor
+	Crie um novo método INSERE para a lista ordenada de tal modo que a lista fique em ordem decrescente.
+'''
